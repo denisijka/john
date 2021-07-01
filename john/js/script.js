dab3e98813df93c95c7f28b2654528bbdb7b7b14
denis <ovicartel@gmail.com>
2021-07-01 14:27:01 +0300

    _header and burger is ready

--- john/js/script.js
+++ john/js/script.js
@@ -16,7 +16,7 @@
 console.log('hello');
 $(document).ready(function () {
     $('.menu__icon').click(function (event) {
-        $('.menu__icon,.menu__body').toggleClass('active');
+        $('.menu__icon,.menu__body').toggleClass('_active');
         $('body').toggleClass('lock');
     });
 });
